eee33438849cf3715094c45adcefaedfb48eb6d9
Hank Jacobs <hank@weezlabs.com>
2015-11-30 17:49:52 -0800

    Configure staging environment. Add health check route ('/health').

--- ror/app/controllers/application_controller.rb
+++ ror/app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def health_check
+  	render text: ENV['RAILS_ENV']
+  end
+
 end
--- ror/config/routes.rb
+++ ror/config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  get 'test' => 'welcome#index'
+
+  get 'health' => 'application#health_check'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

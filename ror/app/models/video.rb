bda5889e57c8592a7cc9101ace6a981fa37e91e0
koryushka <karetnikov.kirill@gmail.com>
2015-12-09 09:44:16 +0300

    add inclusion validator to mpaa_rating

--- ror/app/models/video.rb
+++ ror/app/models/video.rb
@@ -8,8 +8,8 @@ class Video < ActiveRecord::Base
   validates :description, presence: true
   validates :mpaa_rating, presence: true
   validates :category_id, presence: true
-  validates :mpaa_rating, inclusion: {in: %w(G PG PG-13 R NC-17)},
-            message:  'Must be one of the following strings: "G", "PG", "PG-13", "R", "NC-17"'
+  validates :mpaa_rating, inclusion: {in: %w(G PG PG-13 R NC-17),
+            message:  'Must be one of the following strings: "G", "PG", "PG-13", "R", "NC-17"'}
 
   scope :created_after, -> (date){where('created_at>?', date) }
   scope :created_before, -> (date){where('created_at<?', date) }

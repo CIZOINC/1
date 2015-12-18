afd49782a33ef8c3ef18157189fc623be266ca95
koryushka <karetnikov.kirill@gmail.com>
2015-12-18 12:06:21 +0300

    transcode_notification endpoint returns a 404 if the stream does not exist raw_stream_upload_request checks whether filename query parameter is specified set transcode_status default value to 'submitted' add constraints to transcode_status('submitted' or 'progressing' or 'complete' or 'canceled' or 'error' are permitted) debug request headers and body of transcode_notification endpoint

--- ror/app/models/stream.rb
+++ ror/app/models/stream.rb
@@ -2,6 +2,6 @@ class Stream < ActiveRecord::Base
   belongs_to :video
   validates :link, presence: true
   validates :stream_type, presence: true , inclusion: {in: %w(mp4 hls)}
-  validates :transcode_status, presence: true
+  validates :transcode_status, presence: true, inclusion: {in: %w(submitted progressing complete canceled error)}
 
 end

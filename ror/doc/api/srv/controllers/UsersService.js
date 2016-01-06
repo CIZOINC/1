292d0cdc01bd4af28e9479071ae94edd0822495c
Hank Jacobs <hank@weezlabs.com>
2016-01-05 16:27:10 -0800

    Generate updated documentation.

--- ror/doc/api/srv/controllers/UsersService.js
+++ ror/doc/api/srv/controllers/UsersService.js
@@ -6,7 +6,7 @@ exports.usersGet = function() {
   
   examples['application/json'] = {
   "data" : [ {
-    "birthday" : "2016-01-05T03:07:24.370+0000",
+    "birthday" : "2016-01-06T00:23:17.727+0000",
     "password" : "aeiou",
     "id" : 123,
     "email" : "aeiou"
@@ -24,7 +24,7 @@ exports.usersPost = function(body) {
   var examples = {};
   
   examples['application/json'] = {
-  "birthday" : "2016-01-05T03:07:24.371+0000",
+  "birthday" : "2016-01-06T00:23:17.727+0000",
   "password" : "aeiou",
   "id" : 123,
   "email" : "aeiou"
@@ -41,7 +41,7 @@ exports.usersMeGet = function() {
   var examples = {};
   
   examples['application/json'] = {
-  "birthday" : "2016-01-05T03:07:24.372+0000",
+  "birthday" : "2016-01-06T00:23:17.728+0000",
   "password" : "aeiou",
   "id" : 123,
   "email" : "aeiou"
@@ -58,7 +58,7 @@ exports.usersMePut = function(body) {
   var examples = {};
   
   examples['application/json'] = {
-  "birthday" : "2016-01-05T03:07:24.373+0000",
+  "birthday" : "2016-01-06T00:23:17.729+0000",
   "password" : "aeiou",
   "id" : 123,
   "email" : "aeiou"
@@ -77,7 +77,7 @@ exports.usersMeDelete = function() {
 
   
 }
-exports.usersMeVideosLikesGet = function() {
+exports.usersMeVideosLikedGet = function() {
 
   var examples = {};
   
@@ -88,13 +88,145 @@ exports.usersMeVideosLikesGet = function() {
       "link" : "aeiou",
       "stream_type" : "aeiou"
     } ],
-    "created_at" : "2016-01-05T03:07:24.374+0000",
+    "created_at" : "2016-01-06T00:23:17.730+0000",
     "description" : "aeiou",
     "title" : "aeiou",
     "liked" : true,
+    "seen" : true,
     "tags" : [ "aeiou" ],
+    "skipped" : true,
     "viewable" : true,
-    "updated_at" : "2016-01-05T03:07:24.374+0000",
+    "updated_at" : "2016-01-06T00:23:17.730+0000",
+    "category_id" : 123,
+    "id" : 123,
+    "hero_image_link" : "aeiou",
+    "mpaa_rating" : "aeiou",
+    "view_count" : 123
+  } ]
+};
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.usersMeVideosLikedVideoIdPut = function(videoId) {
+
+  var examples = {};
+  
+
+  
+}
+exports.usersMeVideosLikedVideoIdDelete = function(videoId) {
+
+  var examples = {};
+  
+
+  
+}
+exports.usersMeVideosSeenGet = function() {
+
+  var examples = {};
+  
+  examples['application/json'] = {
+  "data" : [ {
+    "streams" : [ {
+      "transcode_status" : "aeiou",
+      "link" : "aeiou",
+      "stream_type" : "aeiou"
+    } ],
+    "created_at" : "2016-01-06T00:23:17.733+0000",
+    "description" : "aeiou",
+    "title" : "aeiou",
+    "liked" : true,
+    "seen" : true,
+    "tags" : [ "aeiou" ],
+    "skipped" : true,
+    "viewable" : true,
+    "updated_at" : "2016-01-06T00:23:17.733+0000",
+    "category_id" : 123,
+    "id" : 123,
+    "hero_image_link" : "aeiou",
+    "mpaa_rating" : "aeiou",
+    "view_count" : 123
+  } ]
+};
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.usersMeVideosSeenVideoIdPut = function(videoId) {
+
+  var examples = {};
+  
+
+  
+}
+exports.usersMeVideosSkippedGet = function() {
+
+  var examples = {};
+  
+  examples['application/json'] = {
+  "data" : [ {
+    "streams" : [ {
+      "transcode_status" : "aeiou",
+      "link" : "aeiou",
+      "stream_type" : "aeiou"
+    } ],
+    "created_at" : "2016-01-06T00:23:17.735+0000",
+    "description" : "aeiou",
+    "title" : "aeiou",
+    "liked" : true,
+    "seen" : true,
+    "tags" : [ "aeiou" ],
+    "skipped" : true,
+    "viewable" : true,
+    "updated_at" : "2016-01-06T00:23:17.735+0000",
+    "category_id" : 123,
+    "id" : 123,
+    "hero_image_link" : "aeiou",
+    "mpaa_rating" : "aeiou",
+    "view_count" : 123
+  } ]
+};
+  
+
+  
+  if(Object.keys(examples).length > 0)
+    return examples[Object.keys(examples)[0]];
+  
+}
+exports.usersMeVideosSkippedVideoIdPut = function(videoId) {
+
+  var examples = {};
+  
+
+  
+}
+exports.usersMeVideosUnseenGet = function() {
+
+  var examples = {};
+  
+  examples['application/json'] = {
+  "data" : [ {
+    "streams" : [ {
+      "transcode_status" : "aeiou",
+      "link" : "aeiou",
+      "stream_type" : "aeiou"
+    } ],
+    "created_at" : "2016-01-06T00:23:17.736+0000",
+    "description" : "aeiou",
+    "title" : "aeiou",
+    "liked" : true,
+    "seen" : true,
+    "tags" : [ "aeiou" ],
+    "skipped" : true,
+    "viewable" : true,
+    "updated_at" : "2016-01-06T00:23:17.736+0000",
     "category_id" : 123,
     "id" : 123,
     "hero_image_link" : "aeiou",
@@ -114,7 +246,7 @@ exports.usersUserIdGet = function(userId) {
   var examples = {};
   
   examples['application/json'] = {
-  "birthday" : "2016-01-05T03:07:24.374+0000",
+  "birthday" : "2016-01-06T00:23:17.737+0000",
   "password" : "aeiou",
   "id" : 123,
   "email" : "aeiou"
@@ -131,7 +263,7 @@ exports.usersUserIdPut = function(body, userId) {
   var examples = {};
   
   examples['application/json'] = {
-  "birthday" : "2016-01-05T03:07:24.375+0000",
+  "birthday" : "2016-01-06T00:23:17.738+0000",
   "password" : "aeiou",
   "id" : 123,
   "email" : "aeiou"

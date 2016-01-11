71f71ac4bdc7e76f8ee836ff3cb9cb6c639f8dd9
Hank Jacobs <hank@weezlabs.com>
2016-01-11 10:23:26 -0800

    Generate documentation.

--- ror/doc/api/srv/controllers/TrendingService.js
+++ ror/doc/api/srv/controllers/TrendingService.js
@@ -11,19 +11,19 @@ exports.trendingGet = function() {
       "link" : "aeiou",
       "stream_type" : "aeiou"
     } ],
-    "created_at" : "2016-01-06T00:23:17.726+0000",
+    "created_at" : "2016-01-11T18:15:37.744+0000",
     "description" : "aeiou",
     "title" : "aeiou",
     "liked" : true,
     "seen" : true,
     "tags" : [ "aeiou" ],
     "skipped" : true,
+    "mature_content" : true,
     "viewable" : true,
-    "updated_at" : "2016-01-06T00:23:17.726+0000",
+    "updated_at" : "2016-01-11T18:15:37.744+0000",
     "category_id" : 123,
     "id" : 123,
     "hero_image_link" : "aeiou",
-    "mpaa_rating" : "aeiou",
     "view_count" : 123
   } ]
 };

cabcb4b6b4df627c4e61671e063548cd7136bf04
koryushka <karetnikov.kirill@gmail.com>
2016-01-15 12:23:15 +0300

    set default mature_content value to false, skip mature_content presence validation

--- ror/db/schema.rb
+++ ror/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114083840) do
+ActiveRecord::Schema.define(version: 20160115091559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

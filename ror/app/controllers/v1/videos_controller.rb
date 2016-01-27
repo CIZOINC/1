8d2c997fa0494648ef4e23d2d0f300acef66cc34
koryushka <karetnikov.kirill@gmail.com>
2016-01-27 17:43:13 +0300

    Add count query parameter with a max value of 200

--- ror/app/controllers/v1/videos_controller.rb
+++ ror/app/controllers/v1/videos_controller.rb
@@ -43,8 +43,9 @@ class V1::VideosController < V1::ApiController
 
     conditions_str = conditions.join(" AND ")
     @videos = Video.where(conditions_str, arguments).desc_order
-
     @videos = @videos.tagged_with(params[:tags]) unless params[:tags].blank?
+
+    (@current_user && as_admin?) ? (@videos = @videos.limit(limited_videos)) : (@videos = @videos.limit(limited_videos(5)))
 
   end
 
@@ -93,7 +94,7 @@ class V1::VideosController < V1::ApiController
   end
 
   def trending
-    @videos = Video.trending.desc_order
+    @videos = Video.trending.desc_order.limit(200)
     render :index
   end
 
@@ -102,7 +103,7 @@ class V1::VideosController < V1::ApiController
       if @current_user && as_admin?
         @videos = Video.where(deleted_at: nil).full_search(search).desc_order
       else
-        @videos = Video.where("visible = ? AND deleted_at IS NULL", true).full_search(search).limit(1000).desc_order
+        @videos = Video.where("visible = ? AND deleted_at IS NULL", true).full_search(search).limit(200).desc_order
       end
     end
   end
@@ -112,7 +113,7 @@ class V1::VideosController < V1::ApiController
     if @current_user && as_admin?
       @videos = Video.where("featured = ? AND deleted_at IS NULL", true).order_by_featured
     else
-      @videos = Video.where("featured = ? AND visible = ? AND deleted_at IS NULL", true, true).limit(1000).order_by_featured
+      @videos = Video.where("featured = ? AND visible = ? AND deleted_at IS NULL", true, true).limit(200).order_by_featured
     end
   end
 
@@ -132,6 +133,10 @@ class V1::VideosController < V1::ApiController
   end
 
   private
+
+  def limited_videos(limit = nil)
+    params[:count].blank? ? limit : (params[:count].to_i > 5 ? 5 : params[:count].to_i)
+  end
 
   def set_region
     @region = "us-east-1"

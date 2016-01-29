7d83374cd50ec853d9db006d5702dc4ab4efe568
koryushka <karetnikov.kirill@gmail.com>
2016-01-29 10:11:17 +0300

    Add count, created_after and created before to query params for seen, liked, skipped and unseen resources

--- ror/app/controllers/v1/api_controller.rb
+++ ror/app/controllers/v1/api_controller.rb
@@ -3,9 +3,6 @@ class V1::ApiController < ApplicationController
   before_action :as_admin?, if: :current_user
   helper_method :as_admin?
 
-  def current_user
-    @current_user = User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
 
   private
 
@@ -25,7 +22,9 @@ class V1::ApiController < ApplicationController
     doorkeeper_token.scopes.to_s == 'admin'
   end
 
-
+  def current_user
+    @current_user = User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 
   def nothing(status)
     render nothing: true, status: status

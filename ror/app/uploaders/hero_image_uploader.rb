0f63488f23a000faef061699068707aec3bfbccd
Hank Jacobs <hank@weezlabs.com>
2016-02-04 12:25:07 -0800

    Change image processing to use resize_to_fill and use the original image as the source image

--- ror/app/uploaders/hero_image_uploader.rb
+++ ror/app/uploaders/hero_image_uploader.rb
@@ -15,15 +15,15 @@ class HeroImageUploader < CarrierWave::Uploader::Base
   end
 
   version :large do
-    process resize_to_limit: [1000, 1000]
+    process resize_to_fill: [1000, 1000]
   end
 
-  version :medium, :from_version => :large do
-    process resize_to_limit: [500, 500]
+  version :medium do
+    process resize_to_fill: [500, 500]
   end
 
-  version :thumb, :from_version => :medium do
-    process resize_to_fit: [250, 250]
+  version :thumb do
+    process resize_to_fill: [250, 250]
   end
 
   def extension_white_list

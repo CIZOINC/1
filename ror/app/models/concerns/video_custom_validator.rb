cd7a4332e2547d7fe0051d3c37c90716be40cd31
koryushka <karetnikov.kirill@gmail.com>
2016-02-09 12:22:29 +0300

    prevent user from changing email prevent admin from removing himself as admin prevent admin from removing self account if he is the last admin. move updating user's tokens from controller to model

--- ror/app/models/concerns/video_custom_validator.rb
+++ ror/app/models/concerns/video_custom_validator.rb
@@ -1,7 +1,7 @@
 class VideoCustomValidator < ActiveModel::Validator
   def validate(record)
     if (record.featured && !record.featured_order) || (!record.featured && record.featured_order)
-      record.errors[:base] << "Unable to save record. If featured true, featured_order can not be nil and vice versa"
+      record.errors[:featured_and_featured_order] << "Unable to save record. If featured true, featured_order can not be nil and vice versa"
     end
   end
 end

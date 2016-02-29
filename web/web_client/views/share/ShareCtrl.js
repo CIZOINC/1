f8fc750de432030088afc4892a803bd2a289d8a3
Pavel Klimenko <laconiez@gmail.com>
2016-02-29 09:17:14 +0300

    update sharing icons

--- web/web_client/views/share/ShareCtrl.js
+++ web/web_client/views/share/ShareCtrl.js
@@ -4,11 +4,14 @@ angular
     .controller('ShareCtrl', ShareCtrl);
 
 /* @ngInject */
-function ShareCtrl($scope, $rootScope, $stateParams, _) {
+function ShareCtrl($scope, $rootScope, $stateParams, _, $state) {
     "use strict";
 
     $scope = angular.extend($scope, {
-        video: undefined
+        video: undefined,
+
+        shareVideoItem: shareVideoItem,
+        closeShare: closeShare
     });
 
 
@@ -17,9 +20,24 @@ function ShareCtrl($scope, $rootScope, $stateParams, _) {
         if (videosArray.length) {
             $scope.video = videosArray[0];
         }
-
-        let t=0;
     }
 
+
+    function shareVideoItem(type) {
+        let socialMap = {
+            'facebook': 'https://www.facebook.com/sharer/sharer.php?u=',
+            'google': 'https://plus.google.com/share?url=',
+            'twitter': 'https://twitter.com/home?status=',
+            'reddit': 'https://www.reddit.com/submit?url='
+        };
+
+        let path = `https%3A//staging.cizo.com/public/web_client/index.html#/play/${$scope.video.id}}/0`;
+
+        window.location = socialMap[type] + path;
+    }
+
+    function closeShare() {
+        $state.go('home');
+    }
 }
-ShareCtrl.$inject = ['$scope', '$rootScope', '$stateParams', 'lodash'];
+ShareCtrl.$inject = ['$scope', '$rootScope', '$stateParams', 'lodash', '$state'];

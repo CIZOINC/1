8e6e9895433468c794e88abe95f54b58791bbbac
Hank Jacobs <hank@hankjacobs.com>
2016-03-09 22:16:06 -0800

    Change reset password URL

--- ror/app/mailers/auth/reset_password_mailer.rb
+++ ror/app/mailers/auth/reset_password_mailer.rb
@@ -6,7 +6,7 @@ class Auth::ResetPasswordMailer < Devise::Mailer
   def reset_password_instructions(user, token)
     @email = user.email
     @web_app_url = Rails.env.production? ? "https://www.cizo.com/" : "https://staging.cizo.com/app/"
-    @reset_url = @web_app_url + "#/resetPassword?token=" + token
+    @reset_url = @web_app_url + "#/resetPassword/" + token
 
     mail(to: @email, subject: 'Reset password instructions')
   end

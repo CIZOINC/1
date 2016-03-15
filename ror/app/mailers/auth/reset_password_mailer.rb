0dddb5a291061e54e68ac2df91e8c7c838db2458
koryushka <karetnikov.kirill@gmail.com>
2016-03-15 10:52:55 +0300

    added inline attachments

--- ror/app/mailers/auth/reset_password_mailer.rb
+++ ror/app/mailers/auth/reset_password_mailer.rb
@@ -5,9 +5,11 @@ class Auth::ResetPasswordMailer < Devise::Mailer
 
   def reset_password_instructions(user, token)
     @email = user.email
+
     @web_app_url = Rails.env.production? ? "https://www.cizo.com/" : "https://staging.cizo.com/app/"
     @reset_url = @web_app_url + "#/resetPassword/" + token
-
+    logo  = File.read(Rails.root.join('app','assets', 'images', 'logo.png'))
+    attachments.inline['logo'] = logo
     mail(to: @email, subject: 'Reset password instructions')
   end
 end

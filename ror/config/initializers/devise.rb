90af3ae9ce929f995a2675023e5bf7b2337ed18a
Hank Jacobs <hank@hankjacobs.com>
2016-03-24 18:11:23 -0700

    Use age_range instead of birthday for Facebook login

--- ror/config/initializers/devise.rb
+++ ror/config/initializers/devise.rb
@@ -238,7 +238,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, provider_ignores_state: true, scope: "email, user_birthday", info_fields: "email, birthday"
+  config.omniauth :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, provider_ignores_state: true, scope: "email, public_profile", info_fields: "email, age_range"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

baebaf94cc14fcfc1ae2616ee4a1ca757772968a
Hank Jacobs <hank@hankjacobs.com>
2016-12-06 12:50:06 -0800

    SEN-618: Return 404 if stream does not have a transcode status of ‘completed’. Update documentation.
    This prevents the CDN from caching a 404 if the stream hasn’t finished transcoding

--- ror/config/initializers/carrierwave.rb
+++ ror/config/initializers/carrierwave.rb
@@ -1,6 +1,8 @@
 CarrierWave.configure do |config|
   fog_credentials = {
     provider:              'AWS',                        # required
+    aws_access_key_id:     "",     # required
+    aws_secret_access_key: ""
   }
 
   unless Rails.env.development? || Rails.env.test?
--- ror/doc/api/srv/controllers/AuthorizationService.js
+++ ror/doc/api/srv/controllers/AuthorizationService.js
@@ -8,7 +8,7 @@ exports.oauthFacebookGet = function(accessToken) {
   "access_token" : "aeiou",
   "refresh_token" : "aeiou",
   "scope" : "aeiou",
-  "created_at" : "2016-11-22T18:55:07.019+0000",
+  "created_at" : "2016-12-06T20:46:07.113+0000",
   "expires_in" : 123
 };
   
@@ -26,7 +26,7 @@ exports.oauthTokenPost = function(body) {
   "access_token" : "aeiou",
   "refresh_token" : "aeiou",
   "scope" : "aeiou",
-  "created_at" : "2016-11-22T18:55:07.023+0000",
+  "created_at" : "2016-12-06T20:46:07.116+0000",
   "token_type" : "aeiou",
   "expires_in" : 123
 };
@@ -45,7 +45,7 @@ exports.usersAuthFacebookGet = function() {
   "access_token" : "aeiou",
   "refresh_token" : "aeiou",
   "scope" : "aeiou",
-  "created_at" : "2016-11-22T18:55:07.028+0000",
+  "created_at" : "2016-12-06T20:46:07.120+0000",
   "expires_in" : 123
 };
   
